fabc79874222666924f4de96863a37fc68d2dc7c
GLUG-NITH <mailglug@gmail.com>
2013-02-01 20:44:35 +0530

    added this new page about contribution via git

---
+++ contribute.php
@@ -0,0 +1,136 @@
+<?php 
+	include_once "./includes/header.php";
+?>
+
+<!-- Main Info starts -->
+<div id="main_info_cont">
+	<div id="main_info_left">
+		<div id="main_info_left_in">
+			<div class="content_style1">
+				<div class="content_style1_heading">
+					<h3>What is Wine?</h3>
+				</div>
+				<div class="content_style1_info">
+					<p>Wine is an Open Source implementation of the Windows API on top of X and Unix. Think of it as a compatibility layer for running Windows programs.</p>
+					<p>Wine does not require Microsoft Windows, as it is a completely free alternative implementation of the Windows API consisting of 100% non-Microsoft code, however Wine can optionally use native Windows DLLs if they are available.</p><br />
+					<strong><a href="http://www.winehq.com/">&#187;&#187;Visit Wine HQ</a></strong>
+				</div>
+			</div>
+		
+	<br /><br />
+		
+		<!--	<div class="content_style1">
+				<div class="content_style1_heading">
+					<h3>GluCki</h3>
+				</div>
+				<div class="content_style1_info">
+					<p>GluCki is the official wiki of GNU/Linux Users Group of National Institute of Technology, Hamirpur. It is a collection of free knowledge based materials broadly centred around the GNU/Linux system.</p>
+					<p>Being a wiki, it allows users complete freedom to edit the contents of the site and make their own contributions. All the contents of GluCki are written indigeniously by the members of GLUG NITH, and are mainly intended to address the needs of the NIT Hamirpur fraternity.</p><br />			
+					<strong><a href="http://202.141.130.198/wiki">&#187;&#187;Visit GluCki</a></strong>
+				</div>
+                
+			</div>-->
+		</div>
+	</div>
+		
+	
+		
+	<div id="main_info_center">
+		<div id="main_info_center_in">
+			<div class="content_style2">
+				<div class="content_style2_heading">
+					<h3>Contributing to website</h3>
+				</div>
+				<div class="content_style2_info">
+					<p>
+			Now you can easily make changes and contribute to the website in whatever way you like. Your contribution will help in building the website, keep it updated and allows GLUG-NITH community to extend with mutual collaboration. Moreover you will be learning a very famous industry tool git which is covered in the below few paragraphs.<br/><br/> 	
+                <h2>Brief Introduction to Git and its power</h2>
+                    <div class="line1"></div>
+                    <p>
+                    GLUG-NITH website source code is maintained and organised very beautifully via this famous tool called git. 
+                    This way anyone can make changes to the website with even little knowledge of html, php and javascript. 
+                    Those who know php and other complex stuff can also make modifications to the website source code which will then directly be reflected on the website after approval of the administrator.
+                    </p><p>
+                    You can add new articles to the website, new projects to the website, new information or anything you like. You change its footer, header, color or anything. With this type of management of the website source code everything can be changed in whatever way you like. However your change should be meaningfull and should be accepted by the GLUG community before approval of your peice of code. At the moment we have excluded the images and other heavy stuff to be included and distributed alongwith the website source code just to decrease the size of the package that you will download in the following steps.
+                    </p><p>
+                    Above was just a brief introduction of what you can do with the website source code using git. Following few paragraphs will provide you a more brief introduction of how you can make changes to the website source code using this very powerful tool called git.
+                    </p><br/>
+                    <h2> Installation </h2>
+                      <div class="line1"></div>
+                    <p>
+                    Git like any other software can be installed using your standard package manager as :<br/>
+
+                    <b>sudo apt-get install git</b><br/>
+
+                    in Ubuntu or other Debian distributions.
+<br/>
+                    or 
+<br/>
+     <b>               sudo yum install git</b><br/>
+
+                    in Fedora or other RPM based distributions.
+                    </p><br/>
+                    <h2> The website source code </h2>
+                     <div class="line1"></div>
+                    <p>
+                        The website source code is hosted on github.com. Source code repository is at following URL : <br/>
+                            <a href=https://github.com/glugnith/glug.nith.ac.in>https://github.com/glugnith/glug.nith.ac.in</a>
+                                <br/>
+                                </p><br/>
+                                <h2>How to contribute ?</h2>
+                                 <div class="line1"></div>
+                                <p>
+                                    This is the website source code but you cannot modify it unless you create your own account on <a href=http://github.com>github.com</a> and then fork this repository under your account. By fork, it means a copy of the same website source code for you to make modifications. You will then make changes to this forked website source code and then send a pull request which will be sent to the account on github from whom you forked the repository. In our case this account is <b>glugnith</b>. The glugnith owner on github then will see if your pull request need to be considered or not. In case of a good change or a good pull request, your request will be approved and will be merged with the main repository which is glugnith repository of website source code i.e from whom you forked the repository. The website administrator will then do some little operations to make that github code appear on the main website. Hence, finally your modification will travel to the website alongwith the approval of glug community.  
+                                     </p>
+                                     <br/>
+                                     <p>
+                                     One thing that must be noted here is in whole of this process, git manages everything very efficiently. The whole process will migrate your changes from one place to other, not the whole source code(except first time you clone the repository). So this way alot of bandwidth is saved and you do not need to wait for minutes to download and upload the whole source code if the same had to be accomplished manually without using git. 
+                                     </p>
+                                     <p>
+                                     Apart from this, there are many other advantages of git. Git has all the history of the commits that has been done so far. You can see the git logs for that. You can also change the whole code to an earlier stage some months back or so. 
+                                     </p>
+                                     <p>
+                                     This way you can very elegantly contribute to glug website source code. 
+                                     </p><br/>
+                    
+                   <h2>Starting using it</h2>
+                      <div class="line1"></div>
+                      <p>
+By this time, you might be wondering how to accomplish this whole thing. For that you need to be aware of this tool git. There are several references on the internet that can help you in getting familiar with git and start hacking. Since the website source code is hosted on <a href=http://github.com>Github</a> and they provide a very good guide to start learning and using git, best place to start using git is their <a href=https://help.github.com/articles/set-up-git>BootCamp</a>. This will help you in learning git from scratch and at the same time getting familiar with Github interface.
+                    </p><br/>
+                    <p>
+                     The <a href=https://help.github.com/articles/set-up-git>BootCamp</a> mentioned above will help you in performing basic tasks that should be enough to contribute. For more information and learning more about git and ways to use git, nothing should be better than their official website and documentation <a href=http://git-scm.com/>here</a>.
+                    </p>
+                    
+                    
+                    
+                    
+                    </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+		
+		
+	<div id="main_info_right">
+		<div id="main_info_right_in">
+			<?php 
+				include_once "./includes/main_info_right.php";
+			?>
+		</div>
+	</div>
+	
+	
+	<div class="float_clear">
+	</div>
+		
+</div>
+
+
+<!-- Main Info ends -->
+
+
+<?php
+	include_once "./includes/footer.php";
+?>

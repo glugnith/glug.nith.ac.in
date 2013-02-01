45bd8272c5d6bd40862b10778f60f429cc41c28e
GLUG-NITH <mailglug@gmail.com>
2013-02-01 20:44:15 +0530

    added contribute via git info in updates.

--- includes/main_info_right.php
+++ includes/main_info_right.php
@@ -18,11 +18,12 @@
 			
 			<div id="glug_updates_all">
 				<ul>
-					<li>GLUG NITH now hosts the official mirror servers of <a href="http://glug.nith.ac.in/ubuntu/">ubuntu</a>, <a href="ftp://glug.nith.ac.in/fedora/">fedora</a>, <a href="http://glug.nith.ac.in/archlinux/">archlinux</a> and mirror website of the <a href="http://glug.nith.ac.in/LDP">Linux Documentation Project(LDP)</a>.</li>
+					<li>GLUG NITH currently hosts the official mirror servers of <a href="http://glug.nith.ac.in/ubuntu/">ubuntu</a><!-- <a href="ftp://glug.nith.ac.in/fedora/">fedora</a>, <a href="http://glug.nith.ac.in/archlinux/">archlinux</a> and--> and <a href=http://www.gnu.org/prep/ftp.html>gnu</a>. We also host mirror website of <a href="http://glug.nith.ac.in/LDP">Linux Documentation Project(LDP)</a>.</li>
 					<li>Currently we are the only GNU and mozzila mirror servers from India. Check the mirror list of gnu <a href="http://www.gnu.org/prep/ftp.html">here</a> and mozilla <a href="http://www.mozilla.org/community/mirrors.html">here</a>.</li>
 					
 
-					 <li>If anyone is interested in writing a tutorial, a review, or sharing interesting links, so just type them down and mail to mailglug@gmail.com . We will publish them on GLUG-NITH website.</li> 
+					 <li>If anyone is interested in writing a tutorial, a review, or sharing interesting links, so just type them down and mail to mailglug@gmail.com. We will publish them on GLUG-NITH website.</li> 
+                     <li>Now you can contribute to website and make necessary changes you think should be reflected on the website using git. Our github repository can be found <a href=https://github.com/glugnith/glug.nith.ac.in>here.</a> You can follow this how to guide <a href=/contribute.php>here.</a></li>
 				</ul>
 					
 			</div>

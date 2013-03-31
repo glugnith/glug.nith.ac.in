1cd650a99a197691a6d4f3fd2f4106bd6b8841fb
GLUG-NITH <mailglug@gmail.com>
2013-03-31 13:54:15 +0530

    added tweets to homepage

---
+++ tweet.php
@@ -0,0 +1,10 @@
+<?php
+include "includes/header.php";
+
+?>
+<center><a class="twitter-timeline"  href="https://twitter.com/glugnith"  data-widget-id="315532987880054784">Tweets by @glugnith</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</center>
+<?php
+include "includes/footer.php";
+?>

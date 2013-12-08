7ef32900585bac9a940a4eef1ebe64bf182c601b
GLUG-NITH <mailglug@gmail.com>
2013-12-08 12:45:37 +0530

    removed claims regarding mirror servers

--- includes/main_info_right.php
+++ includes/main_info_right.php
@@ -19,12 +19,12 @@
 			
 			<div id="glug_updates_all">
 				<ul>
-					<li><p align="justify">
-                        GLUG NITH currently hosts the official mirror servers of <a href="/ubuntu/">ubuntu</a><!-- <a href="ftp://glug.nith.ac.in/fedora/">fedora</a>, <a href="http://glug.nith.ac.in/archlinux/">archlinux</a> and--> and <a href=/gnu>gnu</a>. We also host mirror website of <a href="/LDP">Linux Documentation Project(LDP)</a>.
+<!--					<li><p align="justify">
+                        GLUG NITH currently hosts the official mirror servers of <a href="/ubuntu/">ubuntu</a> <a href="ftp://glug.nith.ac.in/fedora/">fedora</a>, <a href="http://glug.nith.ac.in/archlinux/">archlinux</a> and and <a href=/gnu>gnu</a>. We also host mirror website of <a href="/LDP">Linux Documentation Project(LDP)</a>.
                     </p></li>
 					<li><p align="justify">
                     Currently we are the only GNU and ubuntu cd mirror servers from India. Check the mirror list of gnu <a href="http://www.gnu.org/prep/ftp.html">here</a> and ubuntu <a href="https://launchpad.net/ubuntu/+cdmirrors">here</a>.
-                    </p></li>
+                    </p></li> -->
                      <li><p align="justify">
                      Now you can contribute to website and make necessary changes you think should be reflected on the website like adding new articles, using git. Our github repository can be found <a href=https://github.com/glugnith/glug.nith.ac.in>here.</a> You can follow this how to guide about git <a href=/contribute.php>here</a>.
                      </p></li>

402c70736638d2ebd013b27ea8768e674ad8d063
Rohan Jaswal <rohanjaswal2507@gmail.com>
2014-09-16 13:56:21 +0530

    added the link to sfd wikipage of glugnith on homepage

--- index.php
+++ index.php
@@ -62,6 +62,7 @@
 			This year GNU's Linux Users Group of NIT Hamirpur is celebrating Software freedom Day with full enthusiasm. All the FOSS users in the college are very excited for the Event. This Year SFD is being celebrated worldwide on <b>20th September</b>.
             <br />
 			<a href="http://www.softwarefreedomday.org/" target="_blank">&#187;&#187;View the official website of SFD!</a><br>
+			<a href="http://wiki.softwarefreedomday.org/2014/India/Hamirpur/GLUG-NITH" target="_blank">&#187;&#187;Click here to view the SFD wikipage of NIT Hamirpur</a><br>
 			<a href="http://glug.nith.ac.in/sfd/" target="_blank">&#187;&#187;Click here to know more about SFD celebrations at NIT Hamirpur</a><br>
 			<a href="https://www.youtube.com/channel/UC1Ju3xbS53Jm766K_hxVapQ" target="_blank">&#187;&#187;See the promotional Videos of SFD@NITH</a><br>
 		</p>
